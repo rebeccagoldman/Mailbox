2c205ebd9c1ad4a5a6459082e310b710e2dd25e5
Your Name <rebecca@anyroad.com>
2015-05-23 23:23:58 -0700

    IT WORKS

--- swipe/ViewController.swift
+++ swipe/ViewController.swift
@@ -22,8 +22,10 @@ class ViewController: UIViewController {
     let grayColor = UIColor(red: 178/255, green: 178/255, blue: 178/255, alpha: 1)
     
     @IBOutlet weak var archiveIconView: UIImageView!
-    
+    @IBOutlet weak var listIconView: UIImageView!
+    @IBOutlet weak var laterIconView: UIImageView!
     @IBOutlet weak var deleteIconView: UIImageView!
+    @IBOutlet weak var listView: UIImageView!
     
     var initialCenter: CGPoint!
     var initialArchiveIconCenter: CGPoint!
@@ -33,7 +35,10 @@ class ViewController: UIViewController {
     var transitionToYellow: CGFloat!
     var transitionToBrown: CGFloat!
     var alpha: Int!
+    var didPanRight: Bool!
+    var didPanLeft: Bool!
     
+    @IBOutlet weak var rescheduleView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,6 +46,11 @@ class ViewController: UIViewController {
         actionView.backgroundColor = grayColor
         archiveIconView.alpha = 0
         deleteIconView.alpha = 0
+        listIconView.alpha = 0
+        rescheduleView.alpha = 0
+        listView.alpha = 0
+
+//        laterIconView.alpha = 0
 
     }
 
@@ -49,55 +59,97 @@ class ViewController: UIViewController {
         var velocity = sender.velocityInView(view)
         var location = sender.locationInView(view)
         var translation = sender.translationInView(view)
+       
         
         if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+            didPanRight = velocity.x > 0
+            didPanLeft = velocity.x < 0
+
             
             // define initial center of view
             initialCenter = messageView.center
-            
-            // define color transition poitns
+      
+            // define color transition points for right-swipe
             transitionToGreen = messageView.center.x + 60
             transitionToRed = messageView.center.x + 260
+            
+            // define color transition points for left-swipe
             transitionToYellow = messageView.center.x - 60
+            println("Transition to Yellow: \(transitionToYellow)")
             transitionToBrown = messageView.center.x - 260
 
-            
             println("Gesture began at: \(location)")
             
         } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
             
+            
             messageView.center.x = CGFloat(initialCenter.x + translation.x)
             
             // change to green and show archive icon
-            if messageView.center.x >= transitionToGreen && messageView.center.x < transitionToRed {
+            if didPanRight == true {
                 
-                self.archiveIconView.center.x = translation.x - 25
-                archiveIconView.alpha = 1
-                self.deleteIconView.alpha = 0
+                if messageView.center.x >= transitionToGreen && messageView.center.x < transitionToRed {
+                    
+                    self.archiveIconView.center.x = translation.x - 25
+                    archiveIconView.alpha = 1
+                    self.deleteIconView.alpha = 0
+                    
+                    actionView.backgroundColor = greenColor
+                    
+                // change to red and show cancel icon
+                } else if messageView.center.x >= transitionToRed {
+                    
+                    actionView.backgroundColor = redColor
+                    self.archiveIconView.alpha = 0
+                    self.deleteIconView.alpha = 1
+                    self.deleteIconView.center.x = translation.x - 25
+                    
+                // change to gray and fade in/out archive icon
+                } else if messageView.center.x <= transitionToGreen {
+                    
+                    archiveIconView.alpha = (translation.x / 60)
+                    actionView.backgroundColor = grayColor
+                    
+                }
+            
+            } else if didPanLeft == true {
                 
-                actionView.backgroundColor = greenColor
-                
-            // change to red and show cancel icon
-            } else if messageView.center.x >= transitionToRed {
-                
-                actionView.backgroundColor = redColor
-                self.archiveIconView.alpha = 0
-                self.deleteIconView.alpha = 1
-                self.deleteIconView.center.x = translation.x - 25
-                
-            // change to gray and fade in/out archive icon
-            } else if messageView.center.x <= transitionToGreen {
-                
-                archiveIconView.alpha = (translation.x / 60)
-                actionView.backgroundColor = grayColor
-                
+                if messageView.center.x <= transitionToYellow && messageView.center.x > transitionToBrown {
+                    
+                    laterIconView.center.x = messageView.center.x + 185
+                    println("later icon center: \(translation.x)")
+
+                    laterIconView.alpha = 1
+                    listIconView.alpha = 0
+                    
+                    actionView.backgroundColor = yellowColor
+                    
+                    // change to brown and show list icon
+                } else if messageView.center.x <= transitionToBrown {
+                    
+                    actionView.backgroundColor = brownColor
+                    laterIconView.alpha = 0
+                    listIconView.alpha = 1
+                    listIconView.center.x = messageView.center.x + 185
+                    
+                    // change to gray and fade in/out archive icon
+                } else if messageView.center.x >= transitionToYellow {
+                    
+                    laterIconView.alpha = -(translation.x / 60)
+                    println("alpha: \(-(translation.x / 60))")
+
+                    
+                    actionView.backgroundColor = grayColor
+                    
+                }
             }
+            
             
             
         } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
             
             // if cancel icon is visible, animate icon + message offscreen
-            if messageView.center.x >= transitionToRed && velocity.x > 0  {
+            if deleteIconView.alpha == 1  {
                 
                 UIView.animateWithDuration(0.5, delay: 0.0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
                     
@@ -125,16 +177,47 @@ class ViewController: UIViewController {
                 })
             
             // if gray background is visible, return to original position
-            } else if messageView.center.x <= transitionToGreen {
+            } else if actionView.backgroundColor == grayColor {
                 
                 messageView.center.x = initialCenter.x
-                actionView.backgroundColor = grayColor
                 
             }
             
-            println("Gesture ended at: \(location)")
+            
+            if laterIconView.alpha == 1 && messageView.center.x <= transitionToYellow {
+                
+                UIView.animateWithDuration(0.5, delay: 0.0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+                    
+                    self.messageView.center.x = -1000
+                    self.laterIconView.center.x = -600
+                    self.rescheduleView.alpha = 1
+                    self.actionView.backgroundColor = self.yellowColor
+                    
+                    }, completion: nil)
+                
+                // if list icon is visible, animate icon + message offscreen
+            } else if listIconView.alpha == 1 && messageView.center.x <= transitionToBrown {
+                
+                UIView.animateWithDuration(0.5, delay: 0.0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+                    
+                    self.messageView.center.x = -1000
+                    self.listIconView.center.x = -600
+                    self.listView.alpha = 1
+                    self.actionView.backgroundColor = self.brownColor
+
+
+                    }, completion: nil)
+                
+                // if gray background is visible, return to original position
+            }
+            
+            
+            
             
         }
+        
+        
+        
     }
 
 
